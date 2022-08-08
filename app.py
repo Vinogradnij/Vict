c1d2fd4bfc7cedb9fd09c5c43e8efb968c4e12e5
Vinogradnij <vlud5@mail.ru>
2022-08-08 20:08:27 +0700

    Доработка шапки сайта в base.html, изменение вывода категорий вопросов в index.html с помощью шаблонизатора

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+categories = ['География', 'История', 'Информатика']
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='Главная')
+    return render_template('index.html', title='Викторина', categories=categories)
 
 
 if __name__ == '__main__':
