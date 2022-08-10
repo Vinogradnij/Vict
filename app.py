9e1dc16aeec9bb076053e2f3a413ec79a1516215
Vinogradnij <vlud5@mail.ru>
2022-08-10 17:07:24 +0700

    Создание шаблона для категорий вопросов, добавление обработчиков для каждой из категории

--- app.py
+++ app.py
@@ -3,11 +3,27 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 categories = ['География', 'История', 'Информатика']
+server = 'http://localhost'
 
 
 @app.route('/')
 def index():
     return render_template('index.html', title='Викторина', categories=categories)
+
+
+@app.route('/geography')
+def geography():
+    return render_template('categorie.html', title='География', categorie='География', server=server)
+
+
+@app.route('/history')
+def history():
+    return render_template('categorie.html', title='История', categorie='История', server=server)
+
+
+@app.route('/informatics')
+def informatics():
+    return render_template('categorie.html', title='Информатика', categorie='Информатика', server=server)
 
 
 if __name__ == '__main__':
